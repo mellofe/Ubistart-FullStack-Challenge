f93bf71bcf0f62c0a6fd5a89f29d7b48366166f5
Felipe <felipe.mello@ufv.br>
2022-08-06 16:43:44 -0300

    Inicio da implementação da autenticação

---
+++ Ubistart-FullStack-Challenge/ClientApp/src/app/dtos/AuthenticationRequestDto.ts
@@ -0,0 +1,4 @@
+export class AuthenticationRequestDto{
+    public Email: string;
+    public Password: string
+}
--- Ubistart-FullStack-Challenge/ClientApp/src/app/login/login.component.ts
+++ Ubistart-FullStack-Challenge/ClientApp/src/app/login/login.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { environment } from 'src/environments/environment';
+import { AuthenticationRequestDto } from '../dtos/AuthenticationRequestDto';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public authenticationRequestDto: AuthenticationRequestDto = new AuthenticationRequestDto();
+  private readonly AuthenticateUrl = environment.apiUrl + '/user/authenticate';
+
+  ngOnInit(): void {
+  }
+
+  constructor() { }
+
+  authenticate() {
+    
+  }
+  
 }
