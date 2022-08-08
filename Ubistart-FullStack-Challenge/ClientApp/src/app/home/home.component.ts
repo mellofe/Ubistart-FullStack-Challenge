7b963fce48bd86426dfbb3d3fda6a408e1996cef
Felipe <felipe.mello@ufv.br>
2022-08-07 22:22:23 -0300

    Implementação inicial da lista de tarefas no frontend

--- Ubistart-FullStack-Challenge/ClientApp/src/app/home/home.component.ts
+++ Ubistart-FullStack-Challenge/ClientApp/src/app/home/home.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { LoginComponent } from '../login/login.component';
+
+import { UserDataService } from '../data-services/user.data-service';
+import { TaskDto } from '../dtos/TaskDto';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public Tasks: TaskDto[];
+
+  ngOnInit(): void {
+    if(!LoginComponent.getIsAuthenticated()){
+      this.router.navigate(['']);
+      alert('Faça login para acessar essa página.');
+    }
+  }
+
+  constructor(private userDataService: UserDataService, private router: Router) { }
+  
 }
